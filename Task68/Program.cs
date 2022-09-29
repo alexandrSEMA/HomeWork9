27844b739a8d395bf90acd818602ea9dba291baa
Alexandr <a.semashkev14@gmail.com>
2022-09-29 20:47:14 +0300

    Task 68

---
+++ Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Formyla(m, n)}");
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+int Formyla(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Formyla(m - 1, 1);
+    else
+        return Formyla(m - 1, Formyla(m, n - 1));
+}
